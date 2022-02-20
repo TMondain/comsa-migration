dcc4e3014e196ff8c15460408863725c1317bdbb
TMondain <tmondainmonval@hotmail.com>
2022-02-20 12:09:17 +0000

    initial commit of custom scripts.
    update gitignore

---
+++ scripts/custom_functions.R
@@ -0,0 +1,25 @@
+## Random functions adapted from Finch 2017 supp. matt.
+
+# CUSTOM FUNCTIONS FROM FINCH 2017 ---------------------------------------------------------
+# Mean inter-individual distance
+meanDist <- function(lon, lat){
+  median(lower(distm(cbind(lon, lat), fun = distHaversine)))
+}
+
+# Mantel correlation score
+# r value
+mantelCor <- function(lon1, lat1, lon2, lat2){
+  out <- mantel(lower(distm(cbind(lon1, lat1), fun = distHaversine)) ~
+                  lower(distm(cbind(lon2, lat2), fun = distHaversine)))
+  return(out[c(1)])
+}
+
+# p value
+mantelCorP <- function(lon1, lat1, lon2, lat2){
+  out <- mantel(lower(distm(cbind(lon1, lat1), fun = distHaversine)) ~
+                  lower(distm(cbind(lon2, lat2), fun = distHaversine)))
+  return(out[c(4)])
+}
+
+# Center (without scaling)
+center <- function(x){scale(x, scale = F, center = T)}
